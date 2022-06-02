c34db0ccca2f517f102ec0193bd508230daa18d1
Irina Nissinen <irinanissi@gmail.com>
2022-06-02 13:08:09 +0300

    NFT preview card component challenge

---
+++ main.js
@@ -0,0 +1,55 @@
+const list = document.querySelector('.js-challenges')
+
+const challenges = [
+  {
+    'url': '3-column-preview-card-component',
+    'name': '3-column preview card component',
+    'level': 'newbie',
+    'demo': '',
+    'github': ''
+  },
+  {
+    'url': 'qr-code-component',
+    'name': 'QR code component',
+    'level': 'newbie',
+    'demo': '',
+    'github': ''
+  },
+  {
+    'url': 'interactive-rating-component',
+    'name': 'Interactive rating component',
+    'level': 'newbie',
+    'demo': '',
+    'github': ''
+  },
+  {
+    'url': 'stats-preview-card-component',
+    'name': 'Stats preview card component',
+    'level': 'newbie',
+    'demo': '',
+    'github': ''
+  },
+  {
+    'url': 'nft-preview-card-component',
+    'name': 'NFT preview card component',
+    'level': 'newbie',
+    'demo': '',
+    'github': ''
+  }
+]
+
+for (const challenge of challenges) {
+  const item = document.createElement('li');
+  const link = document.createElement('a');
+
+  link.href = challenge.url
+  link.textContent = challenge.name
+  link.classList.add('challenges-link')
+
+  item.classList.add('challenges-item')
+
+  item.append(link)
+  list.append(item)
+}
+
+console.log(challenges)
